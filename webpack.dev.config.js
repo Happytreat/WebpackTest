bcb4ea06f6f1e91202c2ecbc88881bdc531a4555
Happytreat <cowfats@gmail.com>
2019-07-06 01:34:42 +0800

    Reset new code base to integrate express js into spa

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,79 +1,64 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    'hello-world': './src/hello-world.js',
-    'kiwi': './src/kiwi.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: ''
-    // publicPath: 'https://webpacktesttutorial.com'
-  },
-  mode: 'development',
-  devServer: {
-    contentBase: path.resolve(__dirname, './dist'),
-    index: 'hello-world.js.html',
-    port: 9000
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpg)$/,
-        use: [
-          'file-loader'
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        index: 'index.html',
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [ 'stage-0' ]
+                    }
+                }
+            },
+            {
+                test: /\.hbs$/,
+                use: [
+                    'handlebars-loader'
+                ]
+            }
         ]
-      },
-      {
-        test: /\.css$/,
-        // webpack process loaders from right to left
-        use: [
-          'style-loader', 'css-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader', 'css-loader', 'sass-loader'
-        ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env'],
-            plugins: ['transform-class-properties']
-          }
-        }
-      },
-      {
-        test: /\.hbs$/,
-        use: [
-          'handlebars-loader'
-        ]
-      },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Hello world',
+            description: 'Hello world',
+            template: 'src/page-template.hbs'
+        })
     ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Hello-world',
-      description: 'This is my first webpack test run project: hello-world page',
-      template: 'src/page-template.hbs',
-      filename: '../hello-world.html',
-      chunks: ['hello-world'], // chunk name specified in entry point
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Kiwi',
-      description: 'This is my first webpack test run project: kiwi page',
-      template: 'src/page-template.hbs',
-      filename: '../kiwi.html',
-      chunks: ['kiwi'], // chunk name specified in entry point
-    })
-  ]
 };
